19b1a36cc4513c7b934689e9a19716cd99d70001
clgzb9610 <wkim@macalester.edu>
2018-11-26 23:31:13 -0600

    keyword extraction for resume using Rake is implemented

---
+++ resumeProcessor.py
@@ -0,0 +1,38 @@
+import PyPDF2
+from rake_nltk import Rake
+
+def text_extract(path):
+    pdfObj = open(path, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfObj)
+    resumePage = pdfReader.getPage(0)
+    text = resumePage.extractText()
+    return text
+
+resume = text_extract("resumes/sample4.pdf")
+# print(resume)
+fh = open("resume.txt","w")
+fh.write(resume)
+fh.close()
+
+def process_text(file_path):
+    fileIn = open(file_path, 'r')
+    strList = fileIn.readlines()
+    newStr = ""
+    for i in strList:
+        i = i.strip()
+        if i != '':
+            newStr = newStr + i
+        else:
+            newStr = newStr + " "
+    return newStr
+
+cleaned_string = process_text("resume.txt")
+# print(cleaned_string)
+# fh = open("resume.txt","w")
+# fh.write(cleaned_string)
+# fh.close()
+
+r = Rake(min_length=4)
+r.extract_keywords_from_text(cleaned_string)
+keywords = r.get_ranked_phrases()
+print(keywords)
