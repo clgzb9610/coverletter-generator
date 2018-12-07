52ef20123a9fa9dbd8160873763ddad0b4bebc53
XinyuYang <xyang2@macalester.edu>
2018-12-06 21:43:02 -0600

    Fixed indentation

--- getCoverLetterGUI.py
+++ getCoverLetterGUI.py
@@ -17,99 +17,103 @@ class Page(Frame):
         self.lift()
 
 class introPage(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
 
-       basicControlFrame = Frame(self, bd=5, padx=5, pady=5, relief="groove")
-       basicControlFrame.grid(row=1, column=1)
-       basicControlFrame.pack(anchor=N, fill=BOTH, expand=True, side=LEFT)
-       basicControlFrameTitle = Label(basicControlFrame, text="Creating the introduction", font="Arial 14 bold", padx=5,
+        basicControlFrame = Frame(self, bd=5, padx=5, pady=5, relief="groove")
+        basicControlFrame.grid(row=1, column=1)
+        basicControlFrame.pack(anchor=N, fill=BOTH, expand=True, side=LEFT)
+        basicControlFrameTitle = Label(basicControlFrame, text="Creating the introduction", font="Arial 14 bold", padx=5,
                                       pady=5)
-       basicControlFrameTitle.place(relx=0.5, rely=0.05, anchor=CENTER)
+        basicControlFrameTitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 
+        self.name_input = StringVar()
+        name_entry = Entry(basicControlFrame, textvariable=self.name_input)
 
 
 class bodyPage(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
 
-       self.keywords = StringVar()
-       self.keywords_list = list()
+        self.keywords = StringVar()
+        self.keywords_list = list()
 
-       basicControlFrame = Frame(self, bd=5, padx=5, pady=5, relief="groove")
-       basicControlFrame.grid(row=1, column=1)
-       basicControlFrame.pack(anchor=N, fill=BOTH, expand=True, side=LEFT)
-       basicControlFrameTitle = Label(basicControlFrame, text="Creating the body part", font="Arial 14 bold", padx=5,
+        basicControlFrame = Frame(self, bd=5, padx=5, pady=5, relief="groove")
+        basicControlFrame.grid(row=1, column=1)
+        basicControlFrame.pack(anchor=N, fill=BOTH, expand=True, side=LEFT)
+        basicControlFrameTitle = Label(basicControlFrame, text="Creating the body part", font="Arial 14 bold", padx=5,
                                       pady=5)
-       basicControlFrameTitle.place(relx=0.5, rely=0.05, anchor=CENTER)
+        basicControlFrameTitle.place(relx=0.5, rely=0.05, anchor=CENTER)
 
-       # Load Button
-       masterLoadButton = Button(basicControlFrame, text="Load", command=self.masterLoad)
-       masterLoadButton.place(relx = 0.5, rely = 0.1, anchor = CENTER)
+        # Load Button
+        masterLoadButton = Button(basicControlFrame, text="Load", command=self.masterLoad)
+        masterLoadButton.place(relx = 0.5, rely = 0.1, anchor = CENTER)
 
-       # Keywords list box
-       list_box_label = Label(basicControlFrame, text = "Keywords", font = "Arial 12 bold", padx=5, pady=5)
-       list_box_label.place(relx = 0.5, rely = 0.2, anchor = CENTER)
-       self.list_box = Listbox(basicControlFrame, listvariable=self.keywords, selectmode=MULTIPLE, width=30, height=15)
-       self.list_box.place(relx = 0.5, rely = 0.5, anchor = CENTER)
-       select_btn = Button(basicControlFrame, text="Choose", command=self.masterSelection)
-       select_btn.place(relx=0.5, rely=0.85, anchor = CENTER)
+        # Keywords list box
+        list_box_label = Label(basicControlFrame, text = "Keywords", font = "Arial 12 bold", padx=5, pady=5)
+        list_box_label.place(relx = 0.5, rely = 0.2, anchor = CENTER)
+        self.list_box = Listbox(basicControlFrame, listvariable=self.keywords, selectmode=MULTIPLE, width=30, height=15)
+        self.list_box.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+        select_btn = Button(basicControlFrame, text="Choose", command=self.masterSelection)
+        select_btn.place(relx=0.5, rely=0.85, anchor = CENTER)
 
-       # Error Output
-       self.errorMessage = StringVar()
-       loadErrorOutputLabel = Label(basicControlFrame, textvariable=self.errorMessage, font="Arial 12", padx=5, pady=0)
-       loadErrorOutputLabel.place(relx=0.5, rely = 0.9, anchor = CENTER)
+        # Error Output
+        self.errorMessage = StringVar()
+        loadErrorOutputLabel = Label(basicControlFrame, textvariable=self.errorMessage, font="Arial 12", padx=5, pady=0)
+        loadErrorOutputLabel.place(relx=0.5, rely = 0.9, anchor = CENTER)
 
-       # Information
-       self.finished_template = StringVar()
-
-
-
-   #Loads the file
-   def masterLoad(self):
-       """Asks the user to select a filename of the appropriate type, then it creates the naive bayes
-       network, and displays it."""
-       fileOkay, filename = self._getFilename("Select your resume")
-       if fileOkay:
-           self.keywords.set(extract_keywords(filename))
-           Page.update(self)
-
-   def masterSelection(self):
-       selection = self.list_box.curselection()
-       for i in selection:
-           entry = self.list_box.get(i)
-           self.keywords_list.append(entry)
-           #TODO: creating templates with the keywords given and assign that into self.finished_template
+        # Information
+        self.finished_template = StringVar()
 
 
-   #####################################
-   #private methods
 
-   def _getFilename(self, promptString):
-       """Pops up a dialog box to ask the user for a filename, returns a boolean if the file was chosen well,
-       and the name/path of the file. Otherwise, returns an empty string and a boolean False."""
-       try:
-           filename = tkinter.filedialog.askopenfilename(title=promptString)
-           self.errorMessage.set("")
-       except:
-           self.errorMessage.set("incorrect filename")
-           return False, ""
-       if filename == "":
-           return False, ""
-       return True, filename
+    #Loads the file
+    def masterLoad(self):
+        """Asks the user to select a filename of the appropriate type, then it creates the naive bayes
+        network, and displays it."""
+        fileOkay, filename = self._getFilename("Select your resume")
+        if fileOkay:
+            self.keywords.set(extract_keywords(filename))
+            Page.update(self)
+
+    def masterSelection(self):
+        selection = self.list_box.curselection()
+        for i in selection:
+            entry = self.list_box.get(i)
+            self.keywords_list.append(entry)
+            #TODO: creating templates with the keywords given and assign that into self.finished_template
+
+
+    #####################################
+    #private methods
+
+    def _getFilename(self, promptString):
+        """Pops up a dialog box to ask the user for a filename, returns a boolean if the file was chosen well,
+        and the name/path of the file. Otherwise, returns an empty string and a boolean False."""
+        try:
+            filename = tkinter.filedialog.askopenfilename(title=promptString)
+            self.errorMessage.set("")
+        except:
+            self.errorMessage.set("incorrect filename")
+            return False, ""
+        if filename == "":
+            return False, ""
+        return True, filename
 
 
 class conclusionPage(Page):
-   def __init__(self, *args, **kwargs):
-       Page.__init__(self, *args, **kwargs)
-       label = Label(self, text="Creating the conclusion")
-       label.pack(side="top", fill="both", expand=True)
+    def __init__(self, *args, **kwargs):
+        Page.__init__(self, *args, **kwargs)
+        label = Label(self, text="Creating the conclusion")
+        label.pack(side="top", fill="both", expand=True)
+
 
 class resultPage(Page):
     def __init__(self, *args, **kwargs):
         Page.__init__(self, *args, **kwargs)
         label = Label(self, text="I don't know how to do this part yet")
         label.pack(side="top", fill="both", expand=True)
+
 
 class MainView(Frame):
     def __init__(self, *args, **kwargs):
@@ -141,6 +145,7 @@ class MainView(Frame):
 
         self.p1.show()
 
+
 class getCoverLetterGUI:
 
     def __init__(self, *args, **kwargs):
@@ -165,8 +170,11 @@ class getCoverLetterGUI:
     def masterQuit(self):
         self.root.destroy()
 
+
+
 ###########################################################################################
 # Run the program
+
 
 def runProgram():
     gui = getCoverLetterGUI()
